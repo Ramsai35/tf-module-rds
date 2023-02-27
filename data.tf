73ee3beffb1bc65ccc728ff7d1132c703f2b5d82
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-27 06:20:00 +0530

    advanced subnets

--- data.tf
+++ data.tf
@@ -1,5 +1,16 @@
-data "aws_kms_alias" "key" {
-  name = "alias/roboshop"
+#data "aws_kms_alias" "key" {
+#  name = "alias/roboshop"
+#}
+#
+#data "aws_ssm_parameter" "DB_ADMIN_USER" {
+#  name = "${var.env}.rds.DB_ADMIN_USER"
+#}
+#
+#data "aws_ssm_parameter" "DB_ADMIN_PASS" {
+#  name = "${var.env}.rds.DB_ADMIN_PASS"
+#}
+data "aws_kms_key" "key" {
+  key_id = "alias/roboshop"
 }
 
 data "aws_ssm_parameter" "DB_ADMIN_USER" {
